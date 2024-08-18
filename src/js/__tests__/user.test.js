98ef6f3a670138698a3d7c3bbae399ad115bed36
PrekinII <prekinivan@gmail.com>
2024-08-18 14:57:02 +0500

    Commit_1

--- src/js/__tests__/user.test.js
+++ src/js/__tests__/user.test.js
@@ -1,5 +1,6 @@
 import { loadUser } from '../user';
 import { httpGet } from '../http';
+import health from "../../index";
 
 jest.mock('../http');
 
@@ -13,4 +14,20 @@ test('should call loadUser once', () => {
   const response = loadUser(1);
   expect(response).toEqual({});
   expect(httpGet).toHaveBeenCalledWith('http://server:8080/users/1');
+});
+
+
+const dataList = [
+  [{ name: "Маг", health: 100 }, "healthy"],
+  [{ name: "Маг", health: 51 }, "healthy"],
+  [{ name: "Маг", health: 50 }, "wounded"],
+  [{ name: "Маг", health: 49 }, "wounded"],
+  [{ name: "Маг", health: 15 }, "wounded"],
+  [{ name: "Маг", health: 14 }, "critical"],
+  [{ name: "Маг", health: 1 }, "critical"],
+];
+
+test.each(dataList)("testing function health", (object, expented) => {
+  let result = health(object);
+  expect(result).toEqual(expented);
 });
